4957c2c9f8a9c3d4e8c5c05be3984c429d680567
JANG JUNGWOO <jangisalive1650@gmail.com>
2023-11-06 16:36:42 +0900

    update section3(11/6)

--- all.py
+++ all.py
@@ -1,4 +1,4 @@
-#1. 문제 정의 "구성원의 열 쾌적성을 고려한 공간 최적 온도 설정"
+#1. 문제 정의 "구성원의 열 쾌적성을 고려한 공간 최적 온도 설정 ft.스마트 워치"
 #2. 데이터 수집 "https://view.officeapps.live.com/op/view.aspx?src=https://raw.githubusercontent.com/jangjung-coding/lesson/master/Description%20of%20variables_Liu.docx&wdOrigin=BROWSELINK" + 직접 측정
 #3. 데이터 전처리 및 분석
 '''Units
@@ -15,21 +15,22 @@ import seaborn as sns
 path = 'raw_data_Liu.csv' #전체 데이터 경로
 df= pd.read_csv(path) 
 
-##3-1. 실내 환경만 남겨두기 Location: (1 = indoor; -1 = outdoor)
-df[df["location"]==1] #3297
-df[df["location"]==-1] #546
+##3-1. 실내 환경만 남기고 열 제거 Location: (1 = indoor; -1 = outdoor)
+df[df["location"]==1] #3297 indoor data
+df[df["location"]==-1] #546 outdoor data
 
 df = df[df["location"]==1]
+df = df.drop(columns=['location'])
 
-##3-2. 스마트 워치로 구하기 어려운 피쳐 제거(AnkleT)
-df.columns.nunique() #82
+##3-2. 스마트 워치로 구하기 어려운 피쳐 제거(AnkleT. 밝목온도는 스마트 워치로 측정하기 어려움)
+df.columns.nunique() #81
 columns_to_drop = ['mean.AnkleT_5', 'grad.AnkleT_5', 'sd.AnkleT_5', 'mean.AnkleT_15', 'grad.AnkleT_15', 'sd.AnkleT_15', 'mean.AnkleT_60', 'grad.AnkleT_60', 'sd.AnkleT_60']
 df = df.drop(columns=columns_to_drop)
-df.columns.nunique() #73
+df.columns.nunique() #72
 
 ##3-3. 시간별 피쳐 처리
 '''
-# Environmental features
+- Environmental features(환경 변수)
        'mean.Temperature_60', 'grad.Temperature_60', 'sd.Temperature_60',
        'mean.Temperature_480', 'grad.Temperature_480', 'sd.Temperature_480',
        'mean.Humidity_60', 'grad.Humidity_60', 'sd.Humidity_60',
@@ -38,7 +39,7 @@ df.columns.nunique() #73
        'mean.Winvel_480', 'grad.Winvel_480', 'sd.Winvel_480', 
        'mean.Solar_60', 'grad.Solar_60', 'sd.Solar_60', 
        'mean.Solar_480', 'grad.Solar_480', 'sd.Solar_480',
-# Physiological features
+- Physiological features(생리학적 변수)
        'mean.hr_5', 'grad.hr_5', 'sd.hr_5', 
        'mean.hr_15', 'grad.hr_15', 'sd.hr_15', 
        'mean.hr_60', 'grad.hr_60', 'sd.hr_60', 
@@ -111,39 +112,54 @@ df['mean.act_15'].corr(df['therm_sens']) #0.00
 df['mean.act_60'].corr(df['therm_sens']) #0.00
 ## act feature는 시간별 관계가 떨어지고, therm_sens와도 상관관계가 없고, 손목 움직임이 신체 열을 대표하지 못하는 정적인 사무실 구성원의 특성을 고려하면 제거하는 것이 좋다고 판단
 df = df.drop(columns=['mean.act_5', 'grad.act_5', 'sd.act_5', 'mean.act_15', 'grad.act_15', 'sd.act_15', 'mean.act_60', 'grad.act_60', 'sd.act_60'])
-df.columns.nunique() #64
-## 나머지 feature들은 시간별로 비슷한 경향을 보이므로 60분 데이터만 사용하기로 결정
-df = df.drop(columns=['mean.Temperature_60', 'grad.Temperature_60', 'sd.Temperature_60',
-        'mean.Humidity_60', 'grad.Humidity_60', 'sd.Humidity_60',
+df.columns.nunique() #63
+## 나머지 feature들은 mean, grad, sd 모두 시간별로 비슷한 경향을 보임. 또한 스마트 워치로 온도설정을 하는 주기를 한시간으로 잡아서 mean, grad, sd.60분 데이터만 사용하기로 결정.
+df = df.drop(columns=[
+       'mean.Temperature_480', 'grad.Temperature_480', 'sd.Temperature_480',
+       'mean.Humidity_480', 'grad.Humidity_480', 'sd.Humidity_480', 
+       'mean.Winvel_480', 'grad.Winvel_480', 'sd.Winvel_480', 
+       'mean.Solar_480', 'grad.Solar_480', 'sd.Solar_480',
+       'mean.hr_5', 'grad.hr_5', 'sd.hr_5', 'mean.hr_15', 'grad.hr_15', 'sd.hr_15',
+       'mean.WristT_5', 'grad.WristT_5', 'sd.WristT_5', 'mean.WristT_15', 'grad.WristT_15', 'sd.WristT_15',
+       'mean.PantT_5', 'grad.PantT_5', 'sd.PantT_5', 'mean.PantT_15', 'grad.PantT_15', 'sd.PantT_15'])
+df.columns.nunique() #33
+
+##3-4. 'therm_sens'와 'therm_pref'는 비슷한 의미로 모두 thermal comfort를 나타내는 변수이므로 7가지로 보다 자세한 지표인 'therm_sens'만 사용하기로 결정
+df = df.drop(columns=['therm_pref'])
+df.columns.nunique() #32
+'''
+지금까지 남은 32개의 피쳐는 다음과 같다.
+- Antropometric features(인체 측정 변수)
+        'Sex', 'Age', 'Height', 'Weight'
+- Behavioral features(행동 변수)
+        'Workhr', 'Coffeeintake'
+- Physiological features(생리학적 변수)
+        'mean.hr_60', 'grad.hr_60', 'sd.hr_60', 
+        'mean.WristT_60', 'grad.WristT_60', 'sd.WristT_60', 
+        'mean.PantT_60', 'grad.PantT_60', 'sd.PantT_60'
+- Environmental features(환경 변수) 
+        'mean.Temperature_60', 'grad.Temperature_60', 'sd.Temperature_60', 
+        'mean.Humidity_60', 'grad.Humidity_60', 'sd.Humidity_60', 
         'mean.Winvel_60', 'grad.Winvel_60', 'sd.Winvel_60', 
         'mean.Solar_60', 'grad.Solar_60', 'sd.Solar_60',
-        'mean.hr_60', 'grad.hr_60', 'sd.hr_60',
-        'mean.WristT_60', 'grad.WristT_60', 'sd.WristT_60',
-        'mean.PantT_60', 'grad.PantT_60', 'sd.PantT_60'])
-df.columns.nunique() #43
+- Comfortability features(쾌적성 변수)
+        'therm_sens'
+- Other features(기타 변수)
+        'ID', 'ColdSens', 'ColdExp', 'Vote_time'
+'''
 
-##3-4. 데이터 개인별로 나누기(14명)
+##3-5. 데이터 개인별로 나누기(14명)
 for i in df["ID"].unique():
     subject = df[df["ID"]==i]
     subject.to_csv("subject"+str(i)+".csv")
-##3-5. 여기부터는 개별 subject.py에서 진행
-
+##3-6. 여기부터는 개별 subject.py에서 진행(이상치, 결측치 처리, 모델링, 성능평가, 적용)
+##subject별 결측치와 이상치가 다를것이므로 데이터를 최대한 살리기 위해 각각의 subject.py에서 처리
+##모델링은 3명의 차이가 크고 데이터가 많은 subject를 대상으로 진행 후 나머지 subject에 적용
 #######################################################################
 
 ### 두 피쳐간 상관도를 보는 코드(숫자 + 그림)
 df['mean.hr_60'].corr(df['mean.PantT_60'])
-plt.scatter(df['mean.hr_60'], df['mean.AnkleT_60'], alpha=0.7)
-plt.show()
-
-### X를 위한 선별된 피처 산점도(객관 데이터)
-sns.set(font_scale=1.1) ## 폰트사이즈 조절
-sns.set_style('ticks') ## 축 눈금 표시
-data = df[[
-        'mean.hr_60', 
-        'mean.WristT_60',
-        'mean.AnkleT_60', 
-        'mean.PantT_60']]
-sns.pairplot(data, diag_kind=None,  plot_kws={'alpha':0.2})
+plt.scatter(df['mean.hr_60'], df['mean.PantT_60'], alpha=0.7)
 plt.show()
 
 ### X를 위한 선별된 피처 산점도(객관+주관 데이터)
@@ -153,7 +169,6 @@ data = df[[ # 데이터 선택
     'ColdSens', 'ColdExp',
     'mean.hr_60', 
     'mean.WristT_60',
-    'mean.AnkleT_60', 
     'mean.PantT_60']]
 sns.pairplot(data, diag_kind='kde', markers='o', plot_kws={'alpha':0.4})# Pairplot 생성
 plt.suptitle("Pairplot of Weather Variables", y=1.02)# 그래프 제목 추가
